fec0d99035f2add7e27f3f141288a30d5d126905
alfred <tushartiwari0172@gmail.com>
2023-04-26 15:35:52 +0530

    project-one

--- App.tsx
+++ App.tsx
@@ -1,9 +1,66 @@
-import {SafeAreaView, Text, View} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  useColorScheme,
+} from 'react-native';
 import React from 'react';
 export default () => {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView style={[styles.container, styles.center]}>
+      <View>
+        <Text style={[isDarkMode ? styles.dark : styles.light, styles.text]}>
+          Hello World
+        </Text>
+        <Text style={[isDarkMode ? styles.dark : styles.light, styles.text]}>
+          Hello World
+        </Text>
+        <Text style={[isDarkMode ? styles.dark : styles.light, styles.text]}>
+          Hello World
+        </Text>
+        <Text style={[isDarkMode ? styles.dark : styles.light, styles.text]}>
+          Hello World
+        </Text>
+        <Text style={[isDarkMode ? styles.dark : styles.light, styles.text]}>
+          Hello World
+        </Text>
+        <Text style={[isDarkMode ? styles.dark : styles.light, styles.text]}>
+          Hello World
+        </Text>
+        <Text style={[isDarkMode ? styles.dark : styles.light, styles.text]}>
+          Hello World
+        </Text>
+        <Text style={[isDarkMode ? styles.dark : styles.light, styles.text]}>
+          Hello World
+        </Text>
+        <Text style={[isDarkMode ? styles.dark : styles.light, styles.text]}>
+          Hello World
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: 'red',
+    borderWidth: 5,
+    borderStyle: 'solid',
+    height: '100%',
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dark: {
+    color: 'hotpink',
+  },
+  light: {
+    color: '#333',
+  },
+  text: {
+    fontSize: 22,
+  },
+});
