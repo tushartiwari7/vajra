3623961ebab209e78dfb445e073038dd187619fe
alfred <tushartiwari0172@gmail.com>
2023-04-26 14:46:53 +0530

    First Commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import {SafeAreaView, Text, View} from 'react-native';
+import React from 'react';
+export default () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
