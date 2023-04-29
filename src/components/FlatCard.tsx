843b48eaab22d6d1421c4af6b352b496e522f316
alfred <tushartiwari0172@gmail.com>
2023-04-29 19:55:03 +0530

    custom fonts

--- src/components/FlatCard.tsx
+++ src/components/FlatCard.tsx
@@ -1,24 +1,64 @@
-import {ScrollView, StyleSheet, Text, View, useColorScheme} from 'react-native';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  useColorScheme,
+} from 'react-native';
 
 const FlatCard = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const background = isDarkMode ? styles.bgDark : styles.bgWhite;
+
   return (
-    <View style={styles.box}>
+    <View style={[styles.box, background]}>
       <Text>FlatCard</Text>
     </View>
   );
 };
 
 const FlatCards = () => {
-  console.log('flatcards', useColorScheme());
+  const scaleHeight = ({
+    source,
+    desiredWidth = Dimensions.get('window').width,
+  }: any) => {
+    const {width, height} = Image.resolveAssetSource(source);
+    const useHeight = (desiredWidth / width) * height;
+    return {width: desiredWidth, height: useHeight};
+  };
+
+  const imageSource = '../assets/IMG_20230311_204944.jpg';
+  const imageWidth = 150;
+  const {width, height} = scaleHeight({
+    source: require(imageSource),
+    desiredWidth: imageWidth,
+  });
+
   return (
-    <ScrollView horizontal>
-      <FlatCard />
-      <FlatCard />
-      <FlatCard />
-      <FlatCard />
-      <FlatCard />
-      <FlatCard />
-    </ScrollView>
+    <>
+      <FlatList
+        data={[1, 1, 1, 1, 1, 1, 1, 1, 1]}
+        horizontal
+        renderItem={() => <FlatCard />}
+        ItemSeparatorComponent={() => <View style={styles.gap}></View>}
+      />
+      <View style={styles.card}>
+        <Image
+          resizeMode="contain"
+          source={require(imageSource)}
+          style={{
+            ...{height, width},
+            resizeMode: 'contain',
+          }}
+        />
+        <Text style={styles.heading}>Heading Text</Text>
+        <Text style={styles.sub_heading}>Heading Text</Text>
+        <Text style={styles.description}>Heading Text</Text>
+      </View>
+    </>
   );
 };
 
@@ -28,12 +68,41 @@ const styles = StyleSheet.create({
   box: {
     width: 100,
     height: 100,
-    backgroundColor: '#111',
+    backgroundColor: '#eee',
     justifyContent: 'center',
     alignItems: 'center',
-    marginRight: 10,
-    elevation: 10,
-    shadowColor: '#ffffff',
-    shadowOpacity: 0,
+    elevation: 1,
+  },
+  gap: {
+    width: 5,
+  },
+  bgDark: {
+    backgroundColor: '#121212',
+  },
+  bgWhite: {
+    backgroundColor: '#fafafa',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  card: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 40,
+    fontFamily: 'sans-serif',
+  },
+  heading: {
+    fontSize: 28,
+    fontFamily: 'Poppins',
+  },
+  sub_heading: {
+    fontSize: 20,
+    fontFamily: 'monospace',
+  },
+  description: {
+    fontSize: 18,
+    fontFamily: 'Source Code Pro',
+    fontWeight: '800',
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -32,10 +32,10 @@ type SectionProps = PropsWithChildren<{
 
 function Section({children, title}: SectionProps): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  console.log(isDarkMode);
+
   return (
     <View style={styles.sectionContainer}>
-      <FlatCards />
-
       <Text
         style={[
           styles.sectionTitle,
@@ -60,7 +60,7 @@ function Section({children, title}: SectionProps): JSX.Element {
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
+  console.log(isDarkMode);
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
@@ -79,6 +79,7 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+          <FlatCards />
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits and see if it works.
@@ -92,7 +93,9 @@ function App(): JSX.Element {
           <Section title="Learn More">
             Read the docs to discover what to do next:
           </Section>
-          <LearnMoreLinks />
+          <ScrollView nestedScrollEnabled>
+            <LearnMoreLinks />
+          </ScrollView>
         </View>
       </ScrollView>
     </SafeAreaView>
